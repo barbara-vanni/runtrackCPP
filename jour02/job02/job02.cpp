48fac8f0730bc33f32835b3498d61b770daa79e3
Dhjali <barbara.vanni@laplateforme.io>
2024-04-30 16:20:05 +0200

    [ADD] job02

---
+++ jour02/job02/job02.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    switch(n)
+    {
+        case 0 : cout << "Nul\n";
+        case 1 :
+        case 2 : cout << "Petit\n";
+                            break;
+        case 3 :
+        case 4 :
+        case 5 : cout << "Moyen\n";
+        default : cout << "Grand\n";
+    }
+
+}
+
+/*  Les résultats pour 
+n = 0   --> Nul , Petit
+n = 1  --> Petit
+n = 4   --> Moyen, Grand
+n = 10 --> Grand
+n = -5 --> Grand
+*/
